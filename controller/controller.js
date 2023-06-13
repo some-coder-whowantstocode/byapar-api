d474187d48174d2f7b7428419d8a250765954777
Rohit <rohit19003@gmail.com>
2023-06-13 19:29:14 +0530

    fixing code

--- controller/controller.js
+++ controller/controller.js
@@ -56,7 +56,7 @@ const deleteproduct = async(req,res)=>{
 }
 
 const getbycreater =async(req,res)=>{
-    const {id} = req.user.userId
+    const id = req.user.userId
     console.log(id)
     const products = await Products.find({createdby:id})
     res.status(200).json(products)
